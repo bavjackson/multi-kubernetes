62affe4e962920d57bebee96784a41baf767ce77
bavjackson <bavjackson@gmail.com>
2020-06-23 10:41:04 +0100

    fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t bavjackson/multi-client:latest -t bavjackson/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t bavjackson/multi-server:latest -t bavjackson/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t bavjackson/multi-worker:latest -t bavjackson/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t bavjackson/multi-client:latest -t bavjackson/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t bavjackson/multi-server:latest -t bavjackson/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t bavjackson/multi-worker:latest -t bavjackson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push bavjackson/multi-client:latest
 docker push bavjackson/multi-server:latest
@@ -12,5 +12,5 @@ docker push bavjackson/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=bavjackson/multi-server:$SHA
-kubectl set image deployments/cilent-deployment client=bavjackson/multi-client:$SHA
+kubectl set image deployments/client-deployment client=bavjackson/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=bavjackson/multi-worker:$SHA
